7373b6b0770c2fe55cf42bcc9cd2ca3c9b8c5f48
phantom08266 <phantom08266@gmail.com>
2021-09-03 12:34:58 +0900

    카메라 녹화기능 추가된 기본 프로젝트

---
+++ BlueAid-RC/Model/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlueAid_RC.Model
+{
+    public class User
+    {
+        public string userName { get; set; }
+
+        public string userNumber { get; set; }
+
+        public User(string name, string number)
+        {
+            this.userName = name;
+            this.userNumber = number;
+        }
+    }
+}
